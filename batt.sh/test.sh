f6a180f027cc582d09b44cf3d40ee0a5bf0cc923
vidhunroshan05-dotcom <vidhunroshan05@gmail.com>
2026-02-16 15:55:30 +0530

    test

--- batt.sh/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Hello from GitHub Actions!"
-echo "Current branch: $GITHUB_REF"
-echo "Runner OS: $RUNNER_OS"
-date
---
+++ test.sh
@@ -0,0 +1,6 @@
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+echo "<h1>Server 1</h1>" | sudo tee /var/www/html/index.html
